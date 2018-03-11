fe827b6bb61a125cd644036f0643905d4481f44c
Melloow <37271092+Melloow@users.noreply.github.com>
2018-03-11 18:10:46 +0100

    Delete settings.js

--- .gitignore/settings.js
+++
@@ -1,6 +0,0 @@
-{
-	"yt_api_key": "AIzaSyAh5BSwVaT_flOrJo_XW6X5AsNRfTynjrc",
-	"discord_token": "NDIyMDkxOTcwNTAxMTQ4Njkz.DYWvng.Quv6SQugkqGD3Td140VJpJA5-jc",
-	"bot_controller": "&422166108028534785", 
-	"prefix": "!!!"
-}
